083b1dc7614b26d2dc34d47bb11306dc2c159582
Paige Saint Germain <paigejerin@gmail.com>
2025-01-12 20:18:41 -0500

    completed assignment

--- js/assignment.js
+++ js/assignment.js
@@ -1,17 +1,32 @@
 "use strict";
 
 // Create your references to the html elements here
-const ageInputEl = document.getElementById("age-input")
+const ageInputEl = document.getElementById("age-input");
 const submissionBtn = document.getElementById("submission-btn");
+const response = document.getElementById('response');
 
 // create a mutable variable called age and do not assign it a value.
-
+let age;
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
-    
-    // Write your code below
 
+    console.log(age)
+    // Write your code below
+    if (age >= 21) {
+        console.log('You can vote and purchase alcohol.')
+        response.innerHTML = 'You can vote and purchase alcohol.';
+    } else if (age <= 21 && age >= 18) {
+        console.log('You can vote, but you cannot purchase alcohol.')
+        response.innerHTML = 'You can vote, but you cannot purchase alcohol.';
+    } else if (age < 18) {
+        console.log('You cannot vote and you cannot purchase alcohol.')
+        response.innerHTML = 'You cannot vote and you cannot purchase alcohol.';
+    } else {
+        // Handle any weird error a user might come up with.
+        console.log('Error! Please try again.')
+        response.innerHTML = 'You cannot vote and you cannot purchase alcohol.';
+    }
 }
 
 
--- js/index.js
+++ js/index.js
@@ -1,3 +1,60 @@
-// Prevent us from attempting to use variables
-// that are not declared
-"use strict"
+// // Prevent us from attempting to use variables
+// // that are not declared
+// 'use strict';
+
+// // Copy the last code block into your script and change the hero's health points to see what logic gets executed.
+// let healthPoints = Math.floor(Math.random() * 10) + 1; //Goal: Give a random HP
+// console.log(`Hero has ${healthPoints} health points.`);
+
+// if (healthPoints >= 10) {
+//     console.log('Hero is healthy.');
+// } else if (healthPoints < 10 && healthPoints >= 5) {
+//     console.log('Hero is slightly damaged.');
+// } else {
+//     console.log('Hero is close to death.');
+// }
+
+// // Create a couple if statements that are meaningful.
+// let dog = 'Diggy';
+
+// if (dog === 'Denver') {
+//     console.log(`${dog}! Truly a good boy.`);
+// }
+
+// if (dog === 'Aspen') {
+//     console.log(`${dog}... She's alright.`);
+// }
+
+// if (dog === 'Keaton') {
+//     console.log(`${dog}! A very good boy!`);
+// }
+
+// if (dog === 'Diggy') console.log(`${dog}!!!!!!! THE BEST BOY!!`); // Learned a shorthand for simple if statements
+
+
+// // Create a couple if/else statements that are meaningful.
+// let doggo = 'Aspen';
+
+// if (doggo === 'Denver') {
+//     console.log(`${doggo}! Truly a good boy.`);
+// } else {
+//     console.log(`${doggo}... She's alright.`);
+// }
+
+// // Create a couple if/else if/else statements that are meaningful.
+// let grade =  Math.floor(Math.random() * 101); //Goal: Give a random grade 100-0
+// console.log(`You got a ${grade} on your JS test.`)
+
+// if (grade >= 90) {
+//     console.log('You got an A!');
+// } else if (grade >= 80 && grade <= 89) {
+//     console.log('You got a B.');
+// } else if (grade >= 70 && grade <= 79) {
+//     console.log('You got a C.');
+// } else if (grade >= 60 && grade <= 69) {
+//     console.log('You got a D. Maybe work a little harder...');
+// } else if (grade === 0) {
+//     console.log('...how?');
+// } else {
+//     console.log("That's an F... Yikes.");
+// }
